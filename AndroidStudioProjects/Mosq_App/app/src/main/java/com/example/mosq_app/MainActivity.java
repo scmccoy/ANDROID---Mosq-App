5c79175fbaa7c642f57f8ada0dec3bb0c72e52a7
scmccoy <shanemccoypdx@gmail.com>
2015-09-03 09:03:13 -0400

    first commit

---
+++ AndroidStudioProjects/Mosq_App/app/src/main/java/com/example/mosq_app/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.mosq_app;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import static com.example.mosq_app.R.id.*;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about_page:
+                Intent intent = new Intent(this, AboutActivity.class);
+                this.startActivity(intent);
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+        }
+    }
---
+++ AndroidStudioProjects/MosquitoBite/app/src/main/java/org/httpvectorecology/mosquitobite/MainActivity.java
@@ -0,0 +1,262 @@
+package org.httpvectorecology.mosquitobite;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import java.util.Random;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+
+import android.location.Location;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.widget.ProgressBar;
+
+
+public class MainActivity extends Activity implements ConnectionCallbacks,
+        OnConnectionFailedListener {
+    Button btnClosePopup;
+    final Random randomGenerator = new Random();
+    final ArrayList funFactsList = new ArrayList() {{
+        add("Mosquitoes are the deadliest animals on Earth");
+        add("Only female mosquitoes bite humans and animals; males feed on flower nectar");
+        add("Mosquitoes fly at speeds between 1 and 1.5 miles per hour");
+        add("A mosquito's wings beat 300-600 times per second");
+    }};
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private String mFormattedDate;
+    ProgressBar pb;
+    List<MyTask> tasks;
+    private Button btnNewShowLocation;
+    View v;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pb = (ProgressBar) findViewById(R.id.progressBar1);
+        pb.setVisibility(View.INVISIBLE);
+        btnNewShowLocation = (Button) findViewById(R.id.btnNewShowLocation);
+        tasks = new ArrayList<>();
+        if (checkPlayServices()) {
+            buildGoogleApiClient();
+        }
+        btnNewShowLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mLastLocation != null) {
+                    postDataToServer("http://10.0.2.2/index.php");
+                } else {
+                    Toast.makeText(MainActivity.this, "Network isn't available", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void getData() {
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        Calendar c = Calendar.getInstance();
+        System.out.println("Current time =&gt; " + c.getTime());
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        mFormattedDate = df.format(c.getTime());
+        if (mLastLocation != null) {
+            double latitude = mLastLocation.getLatitude();
+            double longitude = mLastLocation.getLongitude();
+        } else {
+            Toast.makeText(getApplicationContext(), "Searching for location... \nMake sure wifi or GPS is turned On", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private PopupWindow popupWin;
+
+    private void customPopupWindow(View v) {
+        try {
+            LayoutInflater inflater = (LayoutInflater) MainActivity.this
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.screen_popup,
+                    (ViewGroup) findViewById(R.id.popup_element));
+            TextView newArray = (TextView) layout.findViewById(R.id.txtViewArrayContent);
+            String funFactString = (String) funFactsList.get(randomGenerator.nextInt(funFactsList.size()));
+            newArray.setText(funFactString);
+            popupWin = new PopupWindow(layout, 750, 650, true);
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.transparent);
+            popupWin.setBackgroundDrawable(new BitmapDrawable(getResources(),
+                    bitmap));
+            popupWin.setOutsideTouchable(true);
+            popupWin.setFocusable(true);
+            popupWin.showAtLocation(layout, Gravity.CENTER, 0, 0);
+            btnClosePopup = (Button) layout.findViewById(R.id.btn_close_popup);
+            btnClosePopup.setOnClickListener(cancel_button_click_listener);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private View.OnClickListener cancel_button_click_listener = new View.OnClickListener() {
+        public void onClick(View v) {
+            popupWin.dismiss();
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem item = menu.add(Menu.NONE, Menu.NONE, 100, "About");
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intentAbout = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intentAbout);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void postDataToServer(String uri) {
+
+        RequestPackage p = new RequestPackage();
+        p.setMethod("POST");
+        p.setUri(uri);
+        p.setParam("longitude", String.valueOf(mLastLocation.getLongitude()));
+        p.setParam("latitude", String.valueOf(mLastLocation.getLatitude()));
+        p.setParam("date", String.valueOf(mFormattedDate));
+        MyTask task = new MyTask();
+        task.execute(p);
+    }
+
+    protected boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+        getData();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed:  ConnectionResult.getErrorCode() = "
+                + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        getData();
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
+    }
+
+    private class MyTask extends AsyncTask<RequestPackage, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            if (tasks.size() == 0) {
+                pb.setVisibility(View.VISIBLE);
+            }
+            tasks.add(this);
+        }
+
+        @Override
+        protected String doInBackground(RequestPackage... params) {
+            String content = HttpManager.getData(params[0]);
+            return content;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            customPopupWindow(v);
+            tasks.remove(this);
+            if (tasks.size() == 0) {
+                pb.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+}
